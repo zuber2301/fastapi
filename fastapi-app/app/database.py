c6bc037fbad8775163032cd6a1d7ede72f40f5b1
zuber2301 <mohammed.zuber@gmail.com>
2024-06-17 19:44:55 +0530

    updated db connection string

--- fastapi-app/app/database.py
+++ fastapi-app/app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "postgresql://user:password@localhost/dbname"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:juber@localhost/db_fastapi"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
